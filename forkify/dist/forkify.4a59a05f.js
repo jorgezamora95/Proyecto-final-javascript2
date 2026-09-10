ff5e2945cad8e04404ea1d32d3cbfece41450990
Jorge Zamora <jzamora@pentafon.com>
2026-09-09 19:14:45 -0600

    Ya quedo fractional

--- forkify/dist/forkify.4a59a05f.js
+++ forkify/dist/forkify.4a59a05f.js
@@ -964,7 +964,7 @@ class RecipeView extends (0, _viewsDefault.default) {
                 <svg class="recipe__icon">
                     <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''}</div>
                 <div class="recipe__description">
                     <span class="recipe__unit">${ing.unit}</span>
                     ${ing.description}
